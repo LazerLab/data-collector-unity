c62c024e98686e36cff69b1c9abf1b088960812a
Isaiah Mann <contact@isaiahmann.com>
2017-04-13 16:16:56 -0400

    Beginning functionality for fetching variables from VS

---
+++ Assets/Scripts/Testing/FetchVariableTest.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VolunteerScience;
+
+public class FetchVariableTest : MonoBehaviour 
+{
+    [SerializeField]
+    string testKey = "testKey";
+
+	// Use this for initialization
+	void Start() 
+	{
+        VariableFetcher fetcher = VariableFetcher.Get;	
+        fetcher.GetValue(testKey, printValue);
+	}
+
+    void printValue(object value)
+    {
+        Debug.Log(value);
+    }
+
+}
