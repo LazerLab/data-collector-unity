ac9805b9f4f24c159d6afe89c6507a95ea07d0d7
Isaiah Mann <contact@isaiahmann.com>
2017-05-15 13:14:45 -0400

    Renaming files and modifying bash script to auto-rename on copy

--- sync-js.sh
+++ sync-js.sh
@@ -1,5 +1,7 @@
+# Copy the player scripts into the WebGL Export template for the Unity project
 cp JS/vs-unity-globals.js Assets/WebGLTemplates/VolunteerScienceTemplate/JS
 cp JS/vs-unity-player.js Assets/WebGLTemplates/VolunteerScienceTemplate/JS
 
-cp JS/vs-unity-globals.js Host
-cp JS/vs-unity-host.js Host
+# Remove the dashes from the filenames for the host (auto-stripped by Volunteer Science)
+cp JS/vs-unity-globals.js Host/vsunityglobals.js
+cp JS/vs-unity-host.js Host/vsunityhost.js
