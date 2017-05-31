bce635fcdb0385f6d941ada80c0fe544e5164146
Isaiah Mann <contact@isaiahmann.com>
2017-05-30 21:48:50 -0400

    Testing loading images

---
+++ Assets/Scripts/Testing/ImageLoadingTest.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Tests the image loading capabilities
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using VolunteerScience;
+
+public class ImageLoadingTest : MonoBehaviour 
+{
+	[SerializeField]
+	string imageURL;
+	[SerializeField]
+	Image image;
+
+	// Use this for initialization
+	void Start() 
+	{
+		FileLoader files = FileLoader.Get;
+		files.LoadImage(imageURL, setImage);
+	}
+
+	void setImage(Sprite Sprite)
+	{
+		image.sprite = Sprite;
+	}
+
+}
---
+++ Assets/Scripts/VolunteerScience/Files/FileLoader.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Loading files
+ * Usage: [no notes]
+ */
+
+namespace VolunteerScience
+{
+	using System;
+	using System.Collections;
+
+	using UnityEngine;
+
+	public class FileLoader : Singleton<FileLoader>
+	{
+		public void LoadImage(string url, Action<Sprite> callback)
+		{
+			StartCoroutine(loadTexture(url, callback));
+		}
+
+		IEnumerator loadTexture(string url, Action<Sprite> callback)
+		{
+			Texture2D texture;
+			texture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+			WWW webRequest = new WWW(url);
+			yield return webRequest;
+			webRequest.LoadImageIntoTexture(texture);
+			Rect textureRect = new Rect(0, 0, texture.width, texture.height);
+			Sprite sprite = Sprite.Create(texture, textureRect, textureRect.center);
+			callback(sprite);
+		}
+
+	}
+
+}
